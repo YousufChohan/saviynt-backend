cb5ed4ac161d738ef0585b8d6ca91c048d348b5a
Yousuf Chohan <buoycho1999@gmail.com>
2024-01-23 07:35:10 -0700

    Modified Code

--- controllers/GetCategory.js
+++ controllers/GetCategory.js
@@ -2,43 +2,45 @@ const path = require("path");
 const Category = require("../models/category");
 
 class GetCategoryController {
-    static async Execute(req, res) {
+  static async Execute(req, res) {
+    console.log("Category Getting API Hit");
 
-        const { id } = req.query;
+    const { id } = req.query;
 
-        if (id) {
-
-            await Category.findOne({ _id: id, isParent: true })
-                .populate({
-                    path: "subCategory",
-                })
-                .then(result => {
-                    res.status(200).json({
-                        message: "Success",
-                        category: result,
-                    });
-                }).catch(err => {
-                    res.status(400).json({
-                        message: "Error Fetching Category"
-                    })
-                })
-        } else {
-            await Category.find({ isParent: true })
-                .populate({
-                    path: "subCategory",
-                })
-                .then(result => {
-                    res.status(200).json({
-                        message: "Success",
-                        category: result,
-                    });
-                }).catch(err => {
-                    res.status(400).json({
-                        message: "Error Fetching Category"
-                    })
-                })
-        }
+    if (id) {
+      await Category.findOne({ _id: id, isParent: true })
+        .populate({
+          path: "subCategory",
+        })
+        .then((result) => {
+          res.status(200).json({
+            message: "Success",
+            category: result,
+          });
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: "Error Fetching Category",
+          });
+        });
+    } else {
+      await Category.find({ isParent: true })
+        .populate({
+          path: "subCategory",
+        })
+        .then((result) => {
+          res.status(200).json({
+            message: "Success",
+            category: result,
+          });
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: "Error Fetching Category",
+          });
+        });
     }
+  }
 }
 
 module.exports = GetCategoryController;
